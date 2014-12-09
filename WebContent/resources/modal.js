3973da4ed2be3704229e340cf487fb184bdf94ad
Connor Cartwright <connorcart@gmail.com>
2014-12-09 17:41:05 +0000

    Working on layout of home
    yes

---
+++ WebContent/resources/modal.js
@@ -0,0 +1,6 @@
+// ON SUBMIT CLEAR ERROR MESSAGE SPAN
+
+// on unfocus of the email field, or on submit of the form, check that the email address is valid - if it isn't don't submit
+// and put a red border and x next to the email field
+
+// if the email/password is not successful then add in empty span to show error message
